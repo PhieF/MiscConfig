5147df2899869873d3510ac07e1db8859f06c6d0
Phoenamandre <phoenamandre@gmail.com>
2018-03-22 21:48:53 +0100

    fix when more than 15 instances

--- Peertube/transparency.php
+++ Peertube/transparency.php
@@ -1,24 +1,47 @@
 <?php
 $url="https://exode.me";
-$followingsstr= file_get_contents($url."/api/v1/server/following");
-$followings = json_decode($followingsstr, true);
-echo "<h2>Following ".sizeof($followings['data'])." instances"."</h2>";
-echo "videos from these instances will be available in exode.me <br /> <br/>";
-foreach($followings['data'] as $follower){
-		echo  $follower['following']['url']."<br />";
-}
+$cont = true;
+$start=0;
+$f = array();
 
+while($cont){
+	$followingsstr= file_get_contents($url."/api/v1/server/following?start=".$start);
+	$followings = json_decode($followingsstr, true);
+	foreach($followings['data'] as $follower){
+			array_push($f,$follower['following']['url']);
+	}
+	$start+=15;
+	if(sizeof($followings['data'])<15)
+	 $cont=false;
+}
+echo "<h2>Following ".sizeof($f)." instances"."</h2>";
+echo "<b>videos from these instances will be available in exode.me</b> <br /> <br/>";
+foreach($f as $follower){
+	echo $follower."<br />";
+}
 
 echo "<br /><br />";
 
-$followersstr= file_get_contents($url."/api/v1/server/followers");
-$followers = json_decode($followersstr, true);
-
-echo "<h2>Followed by ".sizeof($followers['data'])." instances"."</h2>";
-echo "videos of exode.me will be available in these instances<br /> <br/>";
-
-foreach($followers['data'] as $follower){
-		echo  $follower['follower']['url']."<br />";
+$start=0;
+$f = array();
+$cont=true;
+while($cont){
+	$followersstr= file_get_contents($url."/api/v1/server/followers?start=".$start);
+	$followers = json_decode($followersstr, true);
+	foreach($followers['data'] as $follower){
+			array_push($f,$follower['follower']['url']);
+	}
+	$start+=15;
+	if(sizeof($followers['data'])<15)
+	 $cont=false;
 }
 
+echo "<h2>Followed by ".sizeof($f)." instances"."</h2>";
+echo "<b>videos of exode.me will be available in these instances</b><br /> <br/>";
+foreach($f as $follower){
+	echo $follower."<br />";
+}
+
+
+echo "<br /><br /> <br />Ugly code available at : <a href=\"https://github.com/PhieF/MiscConfig/blob/master/Peertube/transparency.php\">https://github.com/PhieF/MiscConfig/blob/master/Peertube/transparency.php</a>";
 ?>
