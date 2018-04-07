10f4fd69ffdb8b38ce20a5ff61bc4487d1781a02
Phie <phie@phie.ovh>
2018-04-07 20:41:29 +0200

    missing mailto

--- Peertube/js-plugin/contact-admin.js
+++ Peertube/js-plugin/contact-admin.js
@@ -4,7 +4,7 @@
 var mail = "your@mail.com";
 var panels = document.getElementsByClassName("panel-block");
 var panel = panels[panels.length-1];
-panel.innerHTML = panel.innerHTML + '<a _ngcontent-c2=""  href="'+mail+'">\
+panel.innerHTML = panel.innerHTML + '<a _ngcontent-c2=""  href="mailto://'+mail+'">\
       <span _ngcontent-c2="" class="icon icon-about"></span>\
       Contact admin\
     </a>';
