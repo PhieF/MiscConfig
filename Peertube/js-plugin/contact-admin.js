0ca11cc4bc2adce32cbe7bd66e1501bb7758f93e
Phie <phie@phie.ovh>
2018-04-07 20:51:40 +0200

    changing icon

--- Peertube/js-plugin/contact-admin.js
+++ Peertube/js-plugin/contact-admin.js
@@ -5,6 +5,6 @@ var mail = "your@mail.com";
 var panels = document.getElementsByClassName("panel-block");
 var panel = panels[panels.length-1];
 panel.innerHTML = panel.innerHTML + '<a _ngcontent-c2=""  href="mailto://'+mail+'">\
-      <span _ngcontent-c2="" class="icon icon-about"></span>\
+      <span _ngcontent-c2="" class="icon icon-contact"></span>\
       Contact admin\
     </a>';
