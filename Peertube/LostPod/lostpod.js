b67d29e05d47317f7fd5be059d4f041eddd2338c
Phie <phie@phie.ovh>
2018-09-19 17:09:36 +0000

    indent

--- Peertube/LostPod/lostpod.js
+++ Peertube/LostPod/lostpod.js
@@ -1,9 +1,6 @@
-//searchbox
+//center searchbox
 var searchBox = document.getElementById('search-video');
 searchBox.parentNode.removeChild(searchBox)
-
 document.getElementsByClassName("header")[0].insertBefore(searchBox, document.getElementsByClassName("header-right")[0])
-
 //remove search icon
-
 document.getElementsByClassName("icon-search")[0].style.display = "none";
