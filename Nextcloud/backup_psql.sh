de0e156f912a2bfb81c5c45205583a4fa2eba2db
Phie <phie@ns312482.ip-188-165-229.eu>
2020-10-22 13:57:32 +0000

    update

--- Nextcloud/backup_psql.sh
+++ Nextcloud/backup_psql.sh
@@ -1,27 +1,27 @@
 
 #!/bin/bash
 
-if [ "$#" -lt "7" ]
+if [ "$#" -lt "6" ]
         then 
-        echo "Usage nextcloud.sh datadir ncdir dbusername dbpassword database dbport dbserver backupdir [protocol]";
+        echo "Usage nextcloud.sh datadir ncdir dbusername database dbport dbserver backupdir [protocol]";
         exit 1
         fi
 protocol=""
-if [ "$#" -gt "8" ]
+if [ "$#" -gt "7" ]
         then 
-        protocol="-e $9"
+        protocol="-e $8"
 else
-        mkdir -p "$8"/data/
+        mkdir -p "$7"/data/
         fi
 
 DATE=`date '+%Y-%m-%d %H%M%S'`
 #requires ~/.pgpass file dbserver:dbport:database:dbusername:dbpassword
-pg_dump --format=custom --file "/tmp/$DATE.dump" -p "$6" -h "$7" -U "$3" "$5"
+pg_dump --format=custom --file "/tmp/$DATE.dump" -p "$5" -h "$6" -U "$3" "$4"
 tar cfJ "/tmp/$DATE.dump.tar.xz" "/tmp/$DATE.dump"
 rm "/tmp/$DATE.dump"
-scp "/tmp/$DATE.dump.tar.xz" "$8/"
+scp "/tmp/$DATE.dump.tar.xz" "$7/"
 rm "/tmp/$DATE.dump.tar.xz"
 mv "$1"/nextcloud*.log* ./
-rsync -av --del --stats "$1" $protocol "$8"/data
-rsync -av --del --stats "$2" $protocol "$8"/nc
+rsync -av --del --stats "$1" $protocol "$7"/data
+rsync -av --del --stats "$2" $protocol "$7"/nc
 mv nextcloud*.log* "$1"/
--- Peertube/backup.sh
+++ Peertube/backup.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
-PEERTUBE_ROOT_FOLDER="/var/www/peertube"
-PEERTUBE_CONFIG=$PEERTUBE_ROOT_FOLDER"/config"
-PEERTUBE_STORAGE=$PEERTUBE_ROOT_FOLDER"/storage"
-DESTINATION_DIR="/mnt/backup500/Peertube"
+if [ "$#" -lt "8" ]
+        then 
+        echo "Usage backup.sh peertube_dir config_dir storage_dir dbusername db_port db_server database backupdir [protocol]";
+        exit 1
+        fi
 
-PEERTUBE_USERNAME="peertube"
-PEERTUBE_DB="peertube_prod"
-PEERTUBE_DB_PORT=5432
-PEERTUBE_DB_HOST="localhost"
+
+PEERTUBE_ROOT_FOLDER=$1
+PEERTUBE_CONFIG=$2
+PEERTUBE_STORAGE=$3
+DESTINATION_DIR=$8
+
+PEERTUBE_USERNAME=$4
+PEERTUBE_DB=$7
+PEERTUBE_DB_PORT=$5
+PEERTUBE_DB_HOST=$6
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
-#load my local variable, not needed by everyone
-if [ -f local.sh ] 
-   then
-   source local.sh
+protocol=""
+if [ "$#" -gt "8" ]
+        then 
+        protocol="-e $9"
+else
+        mkdir -p "$8"/data/
 fi
-
 mkdir -p "$DESTINATION_DIR"
 #save DB
 #requires ~/.pgpass file localhost:5432:peertube-db:peertube-db-username:password
-pg_dump --format=custom --file "$DESTINATION_DIR/peertube $DATE.dump" -p $PEERTUBE_DB_PORT -h $PEERTUBE_DB_HOST -U $PEERTUBE_USERNAME $PEERTUBE_DB
+pg_dump --format=custom --file "/tmp/peertube $DATE.dump" -p $PEERTUBE_DB_PORT -h $PEERTUBE_DB_HOST -U $PEERTUBE_USERNAME $PEERTUBE_DB
+tar cfJ "/tmp/$DATE.dump.tar.xz" "/tmp/peertube $DATE.dump"
+scp "/tmp/$DATE.dump.tar.xz" "$DESTINATION_DIR"
+rm "/tmp/$DATE.dump.tar.xz" "/tmp/peertube $DATE.dump"
 
 #save config + storage
-rsync -av --del --stats "$PEERTUBE_CONFIG" "$DESTINATION_DIR"
-rsync -av --del --stats "$PEERTUBE_STORAGE" "$DESTINATION_DIR"
+rsync -av --del --stats "$PEERTUBE_CONFIG" "$DESTINATION_DIR/data/config"
+rsync -av --del --stats "$PEERTUBE_STORAGE" "$DESTINATION_DIR/data/storage"
