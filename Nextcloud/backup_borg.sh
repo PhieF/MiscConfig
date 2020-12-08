816e9d2a860796a0c23bf8959445cc3f34b3e552
Phie <phie@phie.ovh>
2020-12-08 21:06:56 +0000

    far better way to have a backup

--- Nextcloud/backup_borg.sh
+++ Nextcloud/backup_borg.sh
@@ -10,14 +10,23 @@ protocol=""
 if [ "$#" -gt "6" ]
         then 
         protocol="-e $7"
+        IFS=':' read -r -a array <<< "$6"
+        ssh ${array[0]} "mkdir -p ${array[1]}"
 else
         mkdir -p "$6"/data/
         fi
-
+borg init $6 --encryption=none
 DATE=`date '+%Y-%m-%d %H%M%S'`
-SQLNAME=`basename "$2"`
-echo $6
-mysqldump -u "$3" -p"$4" "$5"  > "/tmp/$SQLNAME.sql"
+BACKUPNAME=`basename "$2"_nextcloud`
+cd /tmp
+mkdir "$BACKUPNAME"
+cd "$BACKUPNAME"
+mkdir nc
+mkdir data
+sudo mount --bind "$1" /tmp/"$BACKUPNAME"/data
+sudo mount --bind "$2" /tmp/"$BACKUPNAME"/nc
+
+mysqldump -u "$3" -p"$4" "$5"  > "dump.sql"
 #XZ_OPT="--threads=8" tar cfJ "/tmp/$DATE.sql.tar.xz" "/tmp/$DATE.sql"
 #rm "/tmp/$DATE.sql"
 #scp "/tmp/$DATE.sql.tar.xz" "$6/"
@@ -25,7 +34,7 @@ mysqldump -u "$3" -p"$4" "$5"  > "/tmp/$SQLNAME.sql"
 mv "$1"/nextcloud*.log* ./
 #rsync -av --del --stats "$1" $protocol "$6"/data
 #rsync -av --del --stats "$2" $protocol "$6"/nc
-borg create -v --stats "$6"::"${DATE}" "$1" "$2" "/tmp/$SQLNAME.sql" --compression=lz4
+borg create -v --stats "$6"::"${DATE}" nc data "dump.sql" --compression=lz4
 rm "/tmp/$SQLNAME.sql"
 mv nextcloud*.log* "$1"/
 borg prune --keep-daily=7 --keep-weekly=4 --keep-monthly=12 "$6"
--- Nextcloud/backup_psql_borg.sh
+++ Nextcloud/backup_psql_borg.sh
@@ -10,15 +10,28 @@ protocol=""
 if [ "$#" -gt "7" ]
         then 
         protocol="-e $8"
+        IFS=':' read -r -a array <<< "$7"
+        ssh ${array[0]} "mkdir -p ${array[1]}"
 else
         mkdir -p "$7"/data/
         fi
-
+borg init $7 --encryption=none
 DATE=`date '+%Y-%m-%d %H%M%S'`
+BACKUPNAME=`basename "$1"`_nextcloud
+cd /tmp
+mkdir "$BACKUPNAME"
+cd "$BACKUPNAME"
+mkdir nc
+mkdir data
+sudo mount --bind "$1" /tmp/"$BACKUPNAME"/data
+sudo mount --bind "$2" /tmp/"$BACKUPNAME"/nc
+
 #requires ~/.pgpass file dbserver:dbport:database:dbusername:dbpassword
-pg_dump --format=custom --file "/tmp/$DATE.dump" -p "$5" -h "$6" -U "$3" "$4"
+pg_dump --format=custom --file "sql.dump" -p "$5" -h "$6" -U "$3" "$4"
 mv "$1"/nextcloud*.log* ./
 
-borg create -v --stats "$7"::"${DATE}" "$1" "$2" "/tmp/$DATE.dump" --compression=lz4
-rm "/tmp/$DATE.dump"
+borg create -v --stats "$7"::"${DATE}" nc data "sql.dump" --compression=lz4
+rm "sql.dump"
 borg prune --keep-daily=7 --keep-weekly=4 --keep-monthly=12 "$7"
+sudo umount /tmp/"$BACKUPNAME"/nc
+sudo umount /tmp/"$BACKUPNAME"/data
