c96beeda04cc044c255c0419a14b30c2ecc60b3b
Phie <phie@phie.ovh>
2021-01-11 13:50:45 +0000

    fix

--- backup_dir_borg.sh
+++ backup_dir_borg.sh
@@ -1,11 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
-
-$parent=`dirname "$1"`
-$name=`basename "$1"`
+IFS=':' read -r -a array <<< "$2"
+ssh ${array[0]} "mkdir -p ${array[1]}" || true
+parent=`dirname "$1"`
+name=`basename "$1"`
 cd $parent
 borg init $2 --encryption=none || true
 DATE=`date '+%Y-%m-%d %H%M%S'`
 
-borg create -v -p --files-cache=mtime,size --stats "$2"::"${DATE}" "$name" --compression=lz4
-borg prune --keep-daily=7 --keep-weekly=4 --keep-monthly=12 "$8"
+borg create -v -p --stats  "$2"::"${DATE}" "$name" --compression=lz4
+borg prune --keep-daily=7 --keep-weekly=4 --keep-monthly=12 "$2"
