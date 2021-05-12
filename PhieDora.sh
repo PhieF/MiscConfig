21ff1064a01e0b0747627d1633f654d2164165d8
PhieF <phie@phie.ovh>
2021-05-12 21:13:23 +0200

    using rpm fusion for H264
    using rpm fusion for H264

--- PhieDora.sh
+++ PhieDora.sh
@@ -51,14 +51,9 @@ sudo dnf install gnome-extensions-app
 
 #h264
 
-sudo dnf config-manager --set-enabled fedora-cisco-openh264
-sudo dnf install gstreamer1-plugin-openh264 mozilla-openh264
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
 
-#activate on firefox
 
-cd ~/.mozilla/firefox
-cd *.default-release
-echo "user_pref(\"media.gmp-gmpopenh264.enabled\", true);" >> prefs.js
-
-cd ~/
 
